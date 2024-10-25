f958a5d94cc853b8d4f9485163b3a654b71d2c2c
WesllenBrasil <gfwesllen@hotmail.com>
2024-10-25 19:14:45 -0300

    Update exercicio04.js

--- exercicio04.js
+++ exercicio04.js
@@ -3,15 +3,15 @@
 
 let numero_1 = 25
 let numero_2 = 10
+let somar = numero_1 + numero_2
+let diminuir = numero_1 - numero_2
+let multiplicar = numero_1 * numero_2
+let dividir = numero_1 / numero_2
 
-console.log
-    (numero_1 - numero_2)
-
-console.log
-    (numero_1 + numero_2)
-
-console.log
-    (numero_1 * numero_2)
-
-console.log
-    (numero_1 / numero_2)
+console.log(
+    (somar),
+        (diminuir),
+            (multiplicar),
+                (dividir)
+)
+    
